a39aa4e7c242a152f82aefe06db25b38e67c8a43
cevadev <carlos.villanueva.altuna@gmail.com>
2024-02-28 16:59:50 -0300

    Software Biblical Biblos - adding hamburger menu funcionality

--- js/dom/sidebar-toggle.js
+++ js/dom/sidebar-toggle.js
@@ -1,8 +1,10 @@
-const menuBar = document.querySelector(".header-nav nav .bx.bx-menu");
+// const menuBar = document.querySelector(".header-nav nav .bx.bx-menu");
+const menuBar = document.querySelector(".sidebar a .bx.bx-menu");
 const sideBar = document.querySelector(".sidebar");
 
 export default function sidebarToggle() {
-  menuBar.addEventListener("click", () => {
+  menuBar.addEventListener("click", (e) => {
+    e.preventDefault();
     sideBar.classList.toggle("close");
   });
 }
